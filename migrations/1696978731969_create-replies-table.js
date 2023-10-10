45ffc3b43bf6432eccc00c22e9de736519b0cfa8
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-10-11 07:00:59 +0800

    add: migration

---
+++ migrations/1696978731969_create-replies-table.js
@@ -0,0 +1,38 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('replies', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    is_delete: {
+      type: 'TEXT',
+      notNull: true,
+      default: false,
+    },
+  });
+  pgm.addConstraint('replies', 'fk_replies.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+  pgm.addConstraint('replies', 'fk_replies.comment_id_comments.id', 'FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('replies', 'fk_replies.owner_users.id');
+  pgm.dropConstraint('replies', 'fk_replies.comment_id_comments.id');
+  pgm.dropTable('replies');
+};
